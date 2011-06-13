e9752f92f69b2f245005e0ad2208425a6cea1a5f
Tracer Andrew <atracer@princeton.edu>
2011-06-13 16:55:16 -0400

    1. fiddling with wireit (see wireit_diagram.html), 2. added images to www/

--- template_test.py
+++ template_test.py
@@ -6,8 +6,6 @@ joinMod = join.join_module()
 joinMod.id = 'joinMod'
 core.register_module(joinMod)
 
-print joinMod.id
-
 # 3 join mods
 joinMod_d1 = dict(module='joinMod',version='0.0', config=[], position=[20,40])
 joinMod_d2 = dict(module='joinMod',version='0.0', config=[], position=[20,60])
@@ -19,6 +17,10 @@ wire2 = dict(source=[1,'output'], target=[2,'input'])
 
 a = core.Template('testy','Template Testing', [joinMod_d1, joinMod_d2, joinMod_d3], [wire1, wire2], 'tas')
 
+
 #wire_it diagram
-wireit.template_to_wireit_diagram(a)
+f = open('wireit_diagram.txt', 'w')
+
+f.write(str(wireit.template_to_wireit_diagram(a)))
+f.close()
 
