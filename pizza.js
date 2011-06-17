04146044c75e82ee093bc8007d44577638b27dc2
Brian Maranville <brian.maranville@nist.gov>
2011-06-17 16:47:42 -0400

    Please add your preferences for toppings to this definition.
    You will be rewarded for your efforts -
    Brian

---
+++ pizza.js
@@ -0,0 +1,17 @@
+var pizza_prefs = {
+    "brian": {
+        "dislikes": [
+            "anchovies"
+        ], 
+        "likes": [
+            "pepperoni", 
+            "sausage", 
+            "mushrooms"
+        ]
+    }, 
+    "ophir": {
+        "dislikes": [
+            "mushrooms"
+        ]
+    }
+};
