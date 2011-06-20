25a0669a997b601084cabc966ec6fc75c1c8f58f
Brian Maranville <brian.maranville@nist.gov>
2011-06-20 16:42:11 -0400

    adding existing offspecular filters to repository

---
+++ reduction/offspecular/FilterableMetaArray.py
@@ -0,0 +1,15 @@
+from MetaArray import MetaArray
+import filters
+from numpy import ndarray
+import copy
+
+class FilterableMetaArray(MetaArray):
+    def __new__(*args, **kwargs):
+        subarr = MetaArray.__new__(*args, **kwargs)
+        subarr.extrainfo = subarr._info[-1]
+        return subarr
+    def filter(self, filtername, *args, **kwargs):
+        return filters.__getattribute__(filtername)(*args, **kwargs).apply(self)
+        
+    def __deepcopy__(self, memo):
+        return FilterableMetaArray(self.view(ndarray).copy(), info = self.infoCopy())
