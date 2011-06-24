9821ce3a4250e0839b04bd44c884ad9b6a8ea1cc
Tracer Andrew <atracer@princeton.edu>
2011-06-24 13:14:03 -0400

    database test

--- apps/tracks/models.py
+++ apps/tracks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+	field1 = models.CharField(max_length=200)
+	field2 = models.IntegerField()
+	def __unicode__(self):
+		return self.field1
