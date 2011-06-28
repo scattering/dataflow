7c1684826c46ca3f3ecf47fa784662c7aa40df65
Brian Maranville <brian.maranville@nist.gov>
2011-06-27 20:14:10 -0400

    removed unneeded helper function from draw_annulus_aa library

--- dataflow/SANS/draw_annulus_aa.py
+++ dataflow/SANS/draw_annulus_aa.py
@@ -1,16 +1,6 @@
 import Image, ImageDraw
 import numpy
 
-def rebin_factor_2d(a, factor):
-    shape = a.shape
-    return a.reshape(shape[0]/factor, factor, shape[1]/factor, factor).mean(1).mean(2)
-    evList = ['a.reshape('] + \
-             ['args[%d],factor[%d],'%(i,i) for i in range(lenShape)] + \
-             [')'] + ['.mean(%d)'%(i+1) for i in range(lenShape)]
-    print ''.join(evList)
-    return eval(''.join(evList))
-
-
 def annular_mask_antialiased(shape, center, inner_radius, outer_radius, background_value=0.0, mask_value=1.0, oversampling=8):
     """
     Takes shape tuple: (x,y) - this is the size of the output image 
