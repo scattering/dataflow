a192567ffb2f87b3c8731146701b31c94a6b7898
Elakian <elakian@gmail.com>
2011-07-06 13:16:35 -0400

    changes in newinstruments to account for some of the changes in filters

---
+++ dataflow/SANS/correct_detector_sensitivity.py
@@ -0,0 +1,46 @@
+"""
+Correct Detector Sensitivity With .DIV file
+"""
+
+from .. import config
+from ..core import Module
+
+def correct_detector_sensitivity_module(id=None, datatype=None, action=None,
+                 version='0.0', fields=[]):
+    """Uses .DIV to peform division in reduction steps"""
+
+    icon = {
+        'URI': config.IMAGES + "correct_detector_sensitivity.png",
+        'terminals': {
+            'input': (0, 10, -1, 0),
+            'output': (20, 10, 1, 0),
+        }
+    }
+    
+    terminals = [
+        dict(id='input',
+             datatype=datatype,
+             use='in',
+             description='data',
+             required=False,
+             multiple=True,
+             ),
+        dict(id='output',
+             datatype=datatype,
+             use='out',
+             description='correct',
+             ),
+    ]
+
+    # Combine everything into a module.
+    module = Module(id=id,
+                  name='Correct Detector Sensitivity',
+                  version=version,
+                  description=action.__doc__,
+                  icon=icon,
+                  terminals=terminals,
+                  fields=fields,
+                  action=action,
+                  )
+
+    return module
