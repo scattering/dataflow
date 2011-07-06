7ddbaeaa35fa0c592face2cfd0e2e0e08fd13969
Elakian <elakian@gmail.com>
2011-07-06 15:47:23 -0400

    adding icons

--- dataflow/SANS/correct_detector_sensitivity.py
+++ dataflow/SANS/correct_detector_sensitivity.py
@@ -4,13 +4,13 @@ Correct Detector Sensitivity With .DIV file
 
 from .. import config
 from ..core import Module
-
+from ..SANS.map_pics import map_pics
 def correct_detector_sensitivity_module(id=None, datatype=None, action=None,
                  version='0.0', fields=[]):
     """Uses .DIV to peform division in reduction steps"""
 
     icon = {
-        'URI': config.IMAGES + "correct_detector_sensitivity.png",
+        'URI': config.IMAGES + map_pics('div'),
         'terminals': {
             'input': (0, 10, -1, 0),
             'output': (20, 10, 1, 0),
--- dataflow/SANS/initial_correction.py
+++ dataflow/SANS/initial_correction.py
@@ -4,13 +4,13 @@ Does initial correction
 
 from .. import config
 from ..core import Module
-
+from ..SANS.map_pics import map_pics
 def initial_correction_module(id=None, datatype=None, action=None,
                  version='0.0', fields=[]):
     """(SAM-BGD)-[tsam/temp](EMP-BGD)"""
 
     icon = {
-        'URI': config.IMAGES + "initial_correction.png",
+        'URI': config.IMAGES + map_pics('initial'),
         'terminals': {
             'input': (0, 10, -1, 0),
             'output': (20, 10, 1, 0),
