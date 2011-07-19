6581089e6413216cad6d6d3e981b4d3739488cd3
Tracer Andrew <atracer@princeton.edu>
2011-07-19 14:52:11 -0400

    TAS wireit diagram

--- dataflow/tas/instruments.py
+++ dataflow/tas/instruments.py
@@ -248,8 +248,8 @@ def TAS_RUN():
         		value['output'][i] = value['output'][i].get_plottable()
     print result
     return result
-hi=TAS_RUN()
+#hi=TAS_RUN()
 #print 'template: ', simplejson.dumps(wireit.template_to_wireit_diagram(template))
 #print ROOT_URL.REPO_ROOT, ROOT_URL.HOMEDIR
-#print simplejson.dumps(wireit.instrument_to_wireit_language(BT7))
+print simplejson.dumps(wireit.instrument_to_wireit_language(BT7))
 print "done"
