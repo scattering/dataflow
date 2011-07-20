4b1dc2140fba1d28e8b8e7ebdeb19304c31b7e4c
Tracer Andrew <atracer@princeton.edu>
2011-07-20 12:52:37 -0400

    andr added to fillDB

--- fillDB.py
+++ fillDB.py
@@ -3,8 +3,15 @@ if len(Instrument.objects.all()) < 1:
 	Instrument.objects.create(Name='bt7',instrument_class = 'tas')
 	Instrument.objects.create(Name='ng3',instrument_class = 'sans')
 	Instrument.objects.create(Name='new_sans_ins',instrument_class = 'sans2')
+	Instrument.objects.create(Name='ANDR',instrument_class='andr')
 if len(Facility.objects.all()) == 0:
 	Facility.objects.create(Name='NCNR')
 if len(Template.objects.all()) == 0:
 	Template.objects.create(Title='testTemplate', user = User.objects.get(id=1))
 
+
+#to add a new instrument, facility, template manually, you can:    
+	# run python manage.py shell
+	# then, as done above... 
+	# from apps.tracks.models import *
+	# MODEL_NAME.objects.create(params) 
