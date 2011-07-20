497e8e3c99c291d786ae634bdae74839823daffc
Elakian <elakian@gmail.com>
2011-07-20 16:03:15 -0400

    new sans lang defs added

--- static/wireit_test/lang_dict.js
+++ static/wireit_test/lang_dict.js
@@ -1,1 +1,1 @@
-var languageDictionary = {'bt7': testLanguage, 'sans': sans_lang, 'refl': testLanguage, 'andr':andrlang, 'tas': tas_lang, 'sans2': sans_new_lang}
+var languageDictionary = {'bt7': testLanguage, 'sans': sans_lang, 'refl': testLanguage, 'andr':andrlang, 'tas': tas_lang, 'sans2': sans_new_lang,'sans_novel': sans_novel}
