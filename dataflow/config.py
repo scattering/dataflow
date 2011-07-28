88c92e6d492434b11d19e6a0bb74db81c93a9ced
Brendan Rowan <rowan.brendan@gmail.com>
2011-07-28 14:05:05 -0400

    image folder renamed

--- dataflow/config.py
+++ dataflow/config.py
@@ -3,7 +3,7 @@ INSTRUMENTS = ['ncnr.tas.bt7']
 MEDIA = "/media/"
 #IMAGES = MEDIA + "images/"  # URI to root of image directory
 IMAGES = "../../static/img/" # different location
-ANDR_FOLDER = "rowantests/"
+ANDR_FOLDER = "offspecular/"
 JSCRIPT = MEDIA + "scripts/"
 NCNR_DATA = "http://www.ncnr.nist.gov/data/"
 
