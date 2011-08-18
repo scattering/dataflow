421913345226e18fd6e4e30e003c708b84bdbe17
Brian Maranville <brian.maranville@nist.gov>
2011-08-18 19:29:01 -0400

    added new auto-resizing image container, also redistributes terminals

--- static/wireit_test/ANDRdefinition2.js
+++ static/wireit_test/ANDRdefinition2.js
@@ -227,7 +227,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -311,7 +311,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [], 
       "name": "Combine"
@@ -362,7 +362,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -423,7 +423,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -481,7 +481,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -571,7 +571,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -718,7 +718,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -796,7 +796,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [], 
       "name": "Append polarization matrix"
@@ -867,7 +867,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [], 
       "name": "Combine polarized"
@@ -918,7 +918,7 @@ var andr2 = {
             "required": false
           }
         ], 
-        "xtype": "WireIt.ImageContainer"
+        "xtype": "AutosizeImageContainer"
       }, 
       "fields": [
         {
@@ -971,4 +971,62 @@ YAHOO.lang.extend(SaveContainer, WireIt.Container, {
         console.log('save click:', e);
         alert('I am saving!');
     }
+});
+
+AutosizeImageContainer = function(opts, layer) {
+    AutosizeImageContainer.superclass.constructor.call(this, opts, layer);
+    var that = this;
+
+    function auto_space(number, width) {
+        // takes a number and places it in the width evenly
+        var locs = [];
+        if (number == 1) {
+            locs.push(Math.round(width / 2));
+        } else {
+            var spacing = width / (number-1);
+            for (var i=0; i<number; i++) {
+                locs.push(Math.round(i*spacing));
+            }
+        }  
+        return locs;
+    }
+    
+    function distribute_terminals(terminals, width, height) {
+        var locs = [];
+        var directions = {
+            'left': {'fit_axis':'top', 'range': width, 'test': function(el) { return el.direction[0] < 0 } },
+            'right': {'fit_axis':'top', 'range': width, 'test': function(el) { return el.direction[0] > 0 } },
+            'top': {'fit_axis':'left', 'range': height, 'test': function(el) { return el.direction[1] < 0 } },
+            'bottom': {'fit_axis':'left', 'range': height, 'test': function(el) { return el.direction[1] > 0 } },
+        }
+        
+        for (var d in directions) {
+            var terms = terminals.filter( directions[d].test );
+            var padding = 4;
+            var term_size = 16;
+            var range = (directions[d].range - term_size) - (2*padding);
+            var locs = auto_space(terms.length, range);
+            for (var i in terms) {
+                var term = terms[i];
+                term.el.style.setProperty(d, (-1 * term_size));
+                term.offsetPosition[d] = (-1 * term_size);
+                term.el.style.setProperty(directions[d].fit_axis, (locs[i] - (term_size/2) + padding));
+                term.offsetPosition[directions[d].fit_axis] = (locs[i] - (term_size/2) + padding);
+            }
+        }
+    }
+    
+    this.image_obj = new Image();
+    var that = this;
+    this.image_obj.onload = function() {
+        // set the element size, and distribute terminals.
+        that.el.style.width = that.image_obj.width + "px";
+        that.el.style.height = that.image_obj.height + "px";
+        distribute_terminals(that.terminals, that.image_obj.width, that.image_obj.height);
+    }
+    this.image_obj.src = opts.image;
+}
+
+YAHOO.lang.extend(AutosizeImageContainer, WireIt.ImageContainer, {
+    xtype: 'AutosizeImageContainer',
 });
