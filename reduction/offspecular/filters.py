e98e43c92fc22f7ec3c329c588303aff29a1819d
Brian Maranville <brian.maranville@nist.gov>
2011-09-15 18:47:13 -0400

    added Asterix modules

--- reduction/offspecular/filters.py
+++ reduction/offspecular/filters.py
@@ -1,4 +1,4 @@
-from numpy import cos, pi, cumsum, arange, ndarray, ones, zeros, array, newaxis, linspace, empty, resize, sin, allclose, zeros_like, linalg, dot
+from numpy import cos, pi, cumsum, arange, ndarray, ones, zeros, array, newaxis, linspace, empty, resize, sin, allclose, zeros_like, linalg, dot, arctan2, float64, histogram2d
 import os, simplejson, datetime, sys, types, xml.dom.minidom
 from copy import deepcopy
 
@@ -6,6 +6,7 @@ from FilterableMetaArray import FilterableMetaArray as MetaArray
 from he3analyzer import wxHe3AnalyzerCollection as He3AnalyzerCollection
 from reduction.formats import load
 import reduction.rebin as reb
+import h5py
 
 class Supervisor():
     """ class to hold rebinned_data objects and increment their reference count """
@@ -91,7 +92,7 @@ class EmptyQxQzGrid(MetaArray):
                     {"name": "monitor"},
                     {"name": "count_time"}]},
             {'CreationStory': creation_story}]
-        data = MetaArray((qzbins, qxbins, 4), info=info)
+        data = MetaArray(zeros((qzbins, qxbins, 4)), info=info)
         return data
     
 def th_2th_combined_dataobj():
@@ -301,6 +302,86 @@ class WiggleCorrection(Filter2D):
 
         return new_data
 
+class AsterixPixelsToTwotheta(Filter2D):
+    """ input array has pixels axis, convert to 
+    two-theta based on distance from sample to detector and
+    center pixel when two-theta motor is set to zero """
+    
+    @autoApplyToList
+    @updateCreationStory
+    def apply(self, data, qzero_pixel = 145., twotheta_offset=0.0, pw_over_d=0.0003411385649):
+        """ pw_over_d is pixel width divided by distance (sample to detector) """
+        new_info = data.infoCopy()
+        # find pixels axis, and replace with two-theta
+        # assuming there are only 2 dimensions of data, looking only at indices 0, 1
+        xpixel_axis = next((i for i in xrange(len(new_info)) if new_info[i]['name'] == 'xpixel'), None) 
+        if xpixel_axis < 0:
+            print "error: no xpixel axis in this dataset"
+            return
+        
+        new_info[xpixel_axis]['name'] = 'twotheta'    
+        if data._info[-1].has_key('state'):
+            twotheta_offset = float(data._info[-1]['state']['A[0]'])
+        pixels = data.axisValues('xpixel')
+        twotheta = arctan2((pixels - qzero_pixel) * pw_over_d, 1.0) * 180./pi + twotheta_offset
+        new_info[xpixel_axis]['values'] = twotheta
+        new_data = MetaArray(data.view(ndarray).copy(), info=new_info)
+        
+        return new_data
+        
+class AsterixTOFToWavelength(Filter2D):
+    """ input array has TOF axis, convert to wavelength
+    based on calibration (depends on distance from source to detector) """
+    
+    @autoApplyToList
+    @updateCreationStory
+    def apply(self, data, wl_over_tof=1.9050372144288577e-5):
+        """ pw_over_d is pixel width divided by distance (sample to detector) """
+        new_info = data.infoCopy()
+        # find pixels axis, and replace with two-theta
+        # assuming there are only 2 dimensions of data, looking only at indices 0, 1
+        tof_axis = next((i for i in xrange(len(new_info)) if new_info[i]['name'] == 'tof'), None) 
+        if tof_axis < 0:
+            print "error: no tof axis in this dataset"
+            return
+        
+        new_info[tof_axis]['name'] = 'wavelength'
+        tof = data.axisValues('tof')
+        wavelength = (tof * wl_over_tof)
+        # shift by half-bin width to align to center of tof bins!
+        wavelength += (tof[1] - tof[0])/2.0 * wl_over_tof
+        # (bins appear to be centered)
+        new_info[tof_axis]['values'] = wavelength
+        new_data = MetaArray(data.view(ndarray).copy(), info=new_info)
+        
+        return new_data
+
+class AsterixShiftData(Filter2D):
+
+    @autoApplyToList
+    @updateCreationStory
+    def apply(self, data, edge_bin = 180):
+        """ Shift 2D dataset along axis 0, also shifting the axisValues
+        along that edge (assuming linear behaviour) 
+        This is useful for time-of-flight data where the low-t data is empty due
+        to spectrum shape, and can be interpreted as the high-t data from the
+        previous pulse.""" 
+        axis = 0
+        new_info = data.infoCopy()
+        old_axis_values = new_info[axis]['values']
+        
+        shifted_data = empty(data.shape)
+        shifted_data[:-edge_bin,:,:] = data[edge_bin:,:,:]
+        shifted_data[-edge_bin:,:,:] = data[:edge_bin,:,:]
+        shifted_axis = zeros(data.shape[axis])
+        dx = old_axis_values[1] - old_axis_values[0]
+        shifted_axis[:-edge_bin] = old_axis_values[edge_bin:]
+        shifted_axis[-edge_bin:] = old_axis_values[:edge_bin] + (old_axis_values[-1] - old_axis_values[0]) + dx
+        
+        new_info[axis]['values'] = shifted_axis
+        new_data = MetaArray(shifted_data, info=new_info)
+        return new_data
+
 class PixelsToTwotheta(Filter2D):
     """ input array has axes theta and pixels:
     output array has axes theta and twotheta.
@@ -487,7 +568,141 @@ class Autogrid(Filter2D):
 #        else:
 #            return new_single(filename, path, auto_PolState, PolState)
 
-        
+def hdf_to_dict(hdf_obj, convert_i1_tostr=True):
+    out_dict = {}
+    for key in hdf_obj:
+        val = hdf_obj[key]
+        if type(val) == h5py.highlevel.Dataset:
+            if (val.value.dtype == 'int8') and (convert_i1_tostr == True):
+                value = val.value.tostring()
+            else:
+                value = val.value 
+            out_dict[key] = value
+        else:
+            out_dict[key] = hdf_to_dict(val)
+    return out_dict
+
+def LoadAsterixRawHDF(filename, path=None):
+    if path == None:
+        path = os.getcwd()
+    hdf_obj = h5py.File(os.path.join(path, filename), mode='r')
+    run_title = hdf_obj.keys()[0]
+    run_obj = hdf_obj[run_title]
+    state = hdf_to_dict(run_obj['ASTERIX'])
+    monitor = hdf_to_dict(run_obj['scalars'])
+    tof = run_obj['ordela_tof_pz']['tof'].value.astype(float64)
+    twotheta_pixel = run_obj['ordela_tof_pz']['X'].value.astype(float64)
+    data = run_obj['ordela_tof_pz']['data'].value.astype(float64)
+    creation_story = "LoadAsterixRawHDF('{fn}')".format(fn=filename)
+    pol_states = {0:'--', 1:'-+', 2:'+-', 3:'++'}
+    output_objs = []
+    for col in range(4):
+        info = [{"name": "tof", "units": "nanoseconds", "values": tof[:-1] },
+            {"name": "xpixel", "units": "pixels", "values": twotheta_pixel[:-1] },
+            {"name": "Measurements", "cols": [
+                    {"name": "counts"},
+                    {"name": "pixels"},
+                    {"name": "monitor"},
+                    {"name": "count_time"}]},
+            {"PolState": pol_states[col], "filename": filename, "start_datetime": None, 
+             "state": state, "CreationStory":creation_story, "path":path}]
+        data_array = zeros((500, 256, 4))
+        data_array[:,:,1] = 1.0 # pixels
+        data_array[:,:,2] = monitor['microamphours_p%d' % (col,)]
+        data_array[:,:,3] = 1.0 # count time
+        data_array[:,:,0] = data[col,:,:]
+        output_objs.append(MetaArray(data_array[:], dtype='float', info=info[:]))
+    hdf_obj.close()
+    return output_objs 
+
+
+def SuperLoadAsterixHDF(filename, path=None, center_pixel = 145.0, wl_over_tof=1.9050372144288577e-5, pixel_width_over_dist = 0.0195458*pi/180.):
+    """ loads an Asterix file and does the most common reduction steps, 
+    giving back a length-4 list of data objects in twotheta-wavelength space,
+    with the low-tof region shifted to the high-tof region """
+    data_objs = LoadAsterixRawHDF(filename, path)
+    tth_converted = AsterixPixelsToTwotheta().apply(data_objs, qzero_pixel=center_pixel, pw_over_d=pixel_width_over_dist)
+    wl_converted = AsterixTOFToWavelength().apply(tth_converted, wl_over_tof=wl_over_tof)
+    shifted = AsterixShiftData().apply(wl_converted, edge_bin=180)
+    return shifted   
+
+def LoadAsterixHDF(filename, path=None, center_pixel = 145.0, wl_over_tof=1.9050372144288577e-5):
+    if path == None:
+        path = os.getcwd()
+    hdf_obj = h5py.File(os.path.join(path, filename))
+    run_title = hdf_obj.keys()[0]
+    run_obj = hdf_obj[run_title]
+    state = hdf_to_dict(run_obj['ASTERIX'])
+    print state
+    monitor = hdf_to_dict(run_obj['scalars'])
+    tof = run_obj['ordela_tof_pz']['tof'].value.astype(float64)
+    twotheta_pixel = run_obj['ordela_tof_pz']['X'].value.astype(float64)
+    data = run_obj['ordela_tof_pz']['data'].value.astype(float64)
+    tof_to_wavelength_conversion = (0.019050372144288577 / 1000.)
+    wavelength = (tof * wl_over_tof)[:-1]
+    # shift by half-bin width to align to center of tof bins!
+    wavelength += (tof[1] - tof[0])/2.0 * wl_over_tof 
+    # (bins appear to be centered)
+    shifted_data = empty(data.shape)
+    shifted_data[:,:320,:] = data[:,-320:,:]
+    shifted_data[:,320:,:] = data[:,:-320,:]
+    shifted_wavelength = zeros(wavelength.shape)
+    shifted_wavelength[:320] = wavelength[-320:]
+    shifted_wavelength[320:] = wavelength[:-320] + (wavelength[-1] + wavelength[0])
+    pixel_width_over_dist = 0.0195458*pi/180.
+    twotheta_offset = float(state['A[0]'])
+    twotheta = arctan2((twotheta_pixel - center_pixel) * pixel_width_over_dist, 1.0) * 180./pi + twotheta_offset
+    print 'tth:', float(state['A[0]'])
+    pol_states = {0:'--', 1:'-+', 2:'+-', 3:'++'}
+    creation_story = "LoadAsterixHDF('{fn}')".format(fn=filename)
+    #wavelength_axis = data_in[:,0,0]
+    #twotheta_axis = data_in[0,:,1]
+    output_objs = []
+    for col in range(4):
+        info = [{"name": "wavelength", "units": "Angstroms", "values": shifted_wavelength },
+            {"name": "twotheta", "units": "degrees", "values": twotheta },
+            {"name": "Measurements", "cols": [
+                    {"name": "counts"},
+                    {"name": "pixels"},
+                    {"name": "monitor"},
+                    {"name": "count_time"}]},
+            {"PolState": pol_states[col], "filename": filename, "start_datetime": None, 
+             "theta": float(state['A[1]']), "det_angle": float(state['A[0]']),
+             "CreationStory":creation_story, "path":path}]
+        data_array = zeros((500, 256, 4))
+        data_array[:,:,1] = 1.0 # pixels
+        data_array[:,:,2] = 1.0 # monitor
+        data_array[:,:,3] = 1.0 # count time
+        data_array[:,:,0] = shifted_data[col,:,:]
+        output_objs.append(MetaArray(data_array[:], dtype='float', info=info[:]))
+    return output_objs 
+
+def LoadAsterixData(filename, path = None):
+    if path == None:
+        path = os.getcwd()
+    pol_states = {2:'--', 3:'-+', 4:'+-', 5:'++'}  
+    creation_story = "LoadAsterixData('{fn}')".format(fn=filename)
+    data_in = loadtxt(os.path.join(path, filename)).reshape(500,256,6)
+    wavelength_axis = data_in[:,0,0]
+    twotheta_axis = data_in[0,:,1]
+    output_objs = []
+    for col in range(2,6):
+        info = [{"name": "wavelength", "units": "Angstroms", "values": wavelength_axis },
+            {"name": "twotheta", "units": "degrees", "values": twotheta_axis },
+            {"name": "Measurements", "cols": [
+                    {"name": "counts"},
+                    {"name": "pixels"},
+                    {"name": "monitor"},
+                    {"name": "count_time"}]},
+            {"PolState": pol_states[col], "filename": filename, "start_datetime": None, 
+             "CreationStory":creation_story, "path":path}]
+        data_array = zeros((500, 256, 4))
+        data_array[:,:,1] = 1.0 # pixels
+        data_array[:,:,2] = 1.0 # monitor
+        data_array[:,:,3] = 1.0 # count time
+        data_array[:,:,0] = data_in[:,:,col]
+        output_objs.append(MetaArray(data_array[:], dtype='float', info=info[:]))
+    return output_objs    
 
 def LoadICPData(filename, path=None, auto_PolState=False, PolState=''):
     """ 
@@ -756,6 +971,81 @@ class CombinePolarized(Filter2D):
         # we end up with a dictionary set of datasets (e.g. {"++": data1, "--": data2} )
         
         return combined_datasets
+
+class TwothetaLambdaToQxQz(Filter2D):
+    """ Figures out the Qx, Qz values of each datapoint
+    and throws them in the correct bin.  If no grid is specified,
+    one is created that covers the whole range of Q in the dataset
+    
+    If autofill_gaps is True, does reverse lookup to plug holes
+    in the output (but pixel count is still zero for these bins)
+    """
+    
+    default_qxqz_gridvals = (-0.003, 0.003, 201, 0, 0.1, 201)
+    
+    def getQxQz (self, theta, twotheta, wavelength = 5.0): 
+        qLength = 2.0 * pi / wavelength
+        tilt = theta - ( twotheta / 2.0 )
+        dq = 2.0 * qLength * sin( ( pi / 180.0 ) * ( twotheta / 2.0 ) )
+        qxOut = dq * sin( pi * tilt / 180.0 )
+        qzOut = dq * cos( pi * tilt / 180.0 )
+        return [qxOut, qzOut]
+    
+    @autoApplyToList
+    #@updateCreationStory
+    def apply(self, data, output_grid=None, theta=None):
+        if output_grid == None:
+            output_grid = EmptyQxQzGrid(*self.default_qxqz_gridvals)
+        else:
+            output_grid = deepcopy(output_grid)
+            
+        if theta == None:
+            if 'theta' in data._info[-1]:
+                theta = data._info[-1]['theta']
+            else:
+                print "can't run without theta!"
+                return
+       
+        wl_array = data.axisValues('wavelength').copy()
+        wl_array.shape = wl_array.shape + (1,)
+        twotheta_array = data.axisValues('twotheta').copy()
+        twotheta_array.shape = (1,) + twotheta_array.shape
+        qxOut, qzOut = self.getQxQz(theta, twotheta_array, wl_array)
+        
+        # getting values from output grid:
+        outgrid_info = output_grid.infoCopy()
+        numcols = len(outgrid_info[2]['cols'])
+        qx_array = output_grid.axisValues('qx')
+        dqx = qx_array[1] - qx_array[0]
+        qz_array = output_grid.axisValues('qz')
+        dqz = qz_array[1] - qz_array[0]
+        framed_array = zeros((qz_array.shape[0]+2, qx_array.shape[0]+2, numcols))
+        target_qx = ((qxOut - qx_array[0])/dqx + 1).astype(int)
+        #return target_qx, qxOut
+        target_qz = ((qzOut - qz_array[0])/dqz + 1).astype(int)
+        target_mask = (target_qx >= 0) * (target_qx < qx_array.shape[0])
+        target_mask *= (target_qz >= 0) * (target_qz < qz_array.shape[0])
+        target_qx_list = target_qx[target_mask]
+        target_qz_list = target_qz[target_mask]
+        #target_qx = target_qx.clip(0, qx_array.shape[0]+1)
+        #target_qz = target_qz.clip(0, qz_array.shape[0]+1)
+        
+        for i, col in enumerate(outgrid_info[2]['cols']):
+            values_to_bin = data[:,:,col['name']][target_mask]
+            outshape = (output_grid.shape[0], output_grid.shape[1])
+            hist2d, xedges, yedges = histogram2d(target_qz_list,target_qx_list, bins = (outshape[0],outshape[1]), range=((0,outshape[0]),(0,outshape[1])), weights=values_to_bin)
+            output_grid[:,:,col['name']] += hist2d
+            #framed_array[target_qz_list, target_qx_list, i] = data[:,:,col['name']][target_mask]
+            
+        #trimmed_array = framed_array[1:-1, 1:-1]
+        #output_grid[:,:] = trimmed_array
+        
+        creation_story = data._info[-1]['CreationStory']
+        new_creation_story = creation_story + ".filter('{0}', {1})".format(self.__class__.__name__, output_grid._info[-1]['CreationStory'])
+        #print new_creation_story
+        output_grid._info[-1] = data._info[-1].copy()
+        output_grid._info[-1]['CreationStory'] = new_creation_story
+        return output_grid
               
 class ThetaTwothetaToQxQz(Filter2D):
     """ Figures out the Qx, Qz values of each datapoint
@@ -794,14 +1084,21 @@ class ThetaTwothetaToQxQz(Filter2D):
         target_qx = ((qxOut - qx_array[0]) / dqx + 1).astype(int)
         #return target_qx, qxOut
         target_qz = ((qzOut - qz_array[0]) / dqz + 1).astype(int)
-        target_qx = target_qx.clip(0, qx_array.shape[0] + 1)
-        target_qz = target_qz.clip(0, qz_array.shape[0] + 1)
+        
+        target_mask = (target_qx >= 0) * (target_qx < qx_array.shape[0])
+        target_mask *= (target_qz >= 0) * (target_qz < qz_array.shape[0])
+        target_qx_list = target_qx[target_mask]
+        target_qz_list = target_qz[target_mask]
+        #target_qx = target_qx.clip(0, qx_array.shape[0]+1)
+        #target_qz = target_qz.clip(0, qz_array.shape[0]+1)
         
         for i, col in enumerate(outgrid_info[2]['cols']):
-            framed_array[target_qz, target_qx, i] = data[:, :, col['name']]
-            
-        trimmed_array = framed_array[1:-1, 1:-1]
-        output_grid[:, :] = trimmed_array
+            values_to_bin = data[:,:,col['name']][target_mask]
+            outshape = (output_grid.shape[0], output_grid.shape[1])
+            hist2d, xedges, yedges = histogram2d(target_qz_list,target_qx_list, bins = (outshape[0],outshape[1]), range=((0,outshape[0]),(0,outshape[1])), weights=values_to_bin)
+            output_grid[:,:,col['name']] += hist2d
+            #framed_array[target_qz_list, target_qx_list, i] = data[:,:,col['name']][target_mask]
+     
         #extra info changed
         creation_story = data._info[-1]['CreationStory']
         new_creation_story = creation_story + ".filter('{0}', {1})".format(self.__class__.__name__, output_grid._info[-1]['CreationStory'])
