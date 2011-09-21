649211d2d8219d8c32a42b7e8c8eb675595397bb
Brian Maranville <brian.maranville@nist.gov>
2011-09-21 15:10:55 -0400

    removed AutosizeImageContainer (moved to lang_common.js)

--- static/wireit_test/ANDRdefinition2.js
+++ static/wireit_test/ANDRdefinition2.js
@@ -1017,7 +1017,7 @@ var andr2 = {
             "direction": [-1,0], 
             "multiple": false, 
             "name": "input", 
-            "offsetPosition": {"left": -12, "top": 16}, 
+            //"offsetPosition": {"left": -12, "top": 16}, 
             "required": true
           }, 
           {
@@ -1041,7 +1041,7 @@ var andr2 = {
             "direction": [1,0], 
             "multiple": true, 
             "name": "output_y", 
-            "offsetPosition": {"right": -16, "top": 40}, 
+            //"offsetPosition": {"right": -16, "top": 40}, 
             "required": false
           }
         ], 
@@ -1289,60 +1289,61 @@ YAHOO.lang.extend(SaveContainer, WireIt.Container, {
     },
 });
 
-AutosizeImageContainer = function(opts, layer) {
-    AutosizeImageContainer.superclass.constructor.call(this, opts, layer);
-    var that = this;
+//AutosizeImageContainer = function(opts, layer) {
+//    AutosizeImageContainer.superclass.constructor.call(this, opts, layer);
+//    var that = this;
 
-    function auto_space(number, width) {
-        // takes a number and places it in the width evenly
-        var locs = [];
-        if (number == 1) {
-            locs.push(Math.round(width / 2));
-        } else {
-            var spacing = width / (number-1);
-            for (var i=0; i<number; i++) {
-                locs.push(Math.round(i*spacing));
-            }
-        }  
-        return locs;
-    }
-    
-    function distribute_terminals(terminals, width, height) {
-        var locs = [];
-        var directions = {
-            'left': {'fit_axis':'top', 'range': width, 'test': function(el) { return el.direction[0] < 0 } },
-            'right': {'fit_axis':'top', 'range': width, 'test': function(el) { return el.direction[0] > 0 } },
-            'top': {'fit_axis':'left', 'range': height, 'test': function(el) { return el.direction[1] < 0 } },
-            'bottom': {'fit_axis':'left', 'range': height, 'test': function(el) { return el.direction[1] > 0 } },
-        }
-        
-        for (var d in directions) {
-            var terms = terminals.filter( directions[d].test );
-            var padding = 4;
-            var term_size = 16;
-            var range = (directions[d].range - term_size) - (2*padding);
-            var locs = auto_space(terms.length, range);
-            for (var i in terms) {
-                var term = terms[i];
-                term.el.style.setProperty(d, (-1 * term_size));
-                term.offsetPosition[d] = (-1 * term_size);
-                term.el.style.setProperty(directions[d].fit_axis, (locs[i] - (term_size/2) + padding));
-                term.offsetPosition[directions[d].fit_axis] = (locs[i] - (term_size/2) + padding);
-            }
-        }
-    }
-    
-    this.image_obj = new Image();
-    var that = this;
-    this.image_obj.onload = function() {
-        // set the element size, and distribute terminals.
-        that.el.style.width = that.image_obj.width + "px";
-        that.el.style.height = that.image_obj.height + "px";
-        distribute_terminals(that.terminals, that.image_obj.width, that.image_obj.height);
-    }
-    this.image_obj.src = opts.image;
-}
+//    function auto_space(number, width) {
+//        // takes a number and places it in the width evenly
+//        var locs = [];
+//        if (number == 1) {
+//            locs.push(Math.round(width / 2));
+//        } else {
+//            var spacing = width / (number-1);
+//            for (var i=0; i<number; i++) {
+//                locs.push(Math.round(i*spacing));
+//            }
+//        }  
+//        return locs;
+//    }
+//    
+//    function distribute_terminals(terminals, width, height) {
+//        var locs = [];
+//        var directions = {
+//            'left': {'fit_axis':'top', 'range': width, 'test': function(el) { return el.direction[0] < 0 } },
+//            'right': {'fit_axis':'top', 'range': width, 'test': function(el) { return el.direction[0] > 0 } },
+//            'top': {'fit_axis':'left', 'range': height, 'test': function(el) { return el.direction[1] < 0 } },
+//            'bottom': {'fit_axis':'left', 'range': height, 'test': function(el) { return el.direction[1] > 0 } },
+//        }
+//        
+//        for (var d in directions) {
+//            var terms = terminals.filter( directions[d].test );
+//            console.log(d, terms);
+//            var padding = 4;
+//            var term_size = 16;
+//            var range = (directions[d].range - term_size) - (2*padding);
+//            var locs = auto_space(terms.length, range);
+//            for (var i in terms) {
+//                var term = terms[i];
+//                term.el.style.setProperty(d, (-1 * term_size));
+//                term.offsetPosition = {d: (-1 * term_size)};
+//                term.el.style.setProperty(directions[d].fit_axis, (locs[i] - (term_size/2) + padding));
+//                term.offsetPosition[directions[d].fit_axis] = (locs[i] - (term_size/2) + padding);
+//            }
+//        }
+//    }
+//    
+//    this.image_obj = new Image();
+//    var that = this;
+//    this.image_obj.onload = function() {
+//        // set the element size, and distribute terminals.
+//        that.el.style.width = that.image_obj.width + "px";
+//        that.el.style.height = that.image_obj.height + "px";
+//        distribute_terminals(that.terminals, that.image_obj.width, that.image_obj.height);
+//    }
+//    this.image_obj.src = opts.image;
+//}
 
-YAHOO.lang.extend(AutosizeImageContainer, WireIt.ImageContainer, {
-    xtype: 'AutosizeImageContainer',
-});
+//YAHOO.lang.extend(AutosizeImageContainer, WireIt.ImageContainer, {
+//    xtype: 'AutosizeImageContainer',
+//});
