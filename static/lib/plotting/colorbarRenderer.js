5bc6c0cd8367fe5ec562b7f3fc189c7732ea7177
Brian Maranville <brian.maranville@nist.gov>
2011-12-06 23:28:44 -0500

    updating the slice test

--- static/lib/plotting/colorbarRenderer.js
+++ static/lib/plotting/colorbarRenderer.js
@@ -1,14 +1,14 @@
 (function($) {
     // inherit from LineRenderer
-    $.jqplot.heatmapRenderer = function(){
+    $.jqplot.colorbarRenderer = function(){
         $.jqplot.LineRenderer.call(this);
     };
     
-    $.jqplot.heatmapRenderer.prototype = new $.jqplot.LineRenderer();
-    $.jqplot.heatmapRenderer.prototype.constructor = $.jqplot.heatmapRenderer;
+    $.jqplot.colorbarRenderer.prototype = new $.jqplot.LineRenderer();
+    $.jqplot.colorbarRenderer.prototype.constructor = $.jqplot.colorbarRenderer;
     
     // called with scope of a series
-    $.jqplot.heatmapRenderer.prototype.init = function(options) {
+    $.jqplot.colorbarRenderer.prototype.init = function(options) {
         // Group: Properties
         //
         
@@ -18,7 +18,7 @@
         this.palette_str = ["rgb(0,0,127)", "rgb(0,0,132)", "rgb(0,0,136)", "rgb(0,0,141)", "rgb(0,0,145)", "rgb(0,0,150)", "rgb(0,0,154)", "rgb(0,0,159)", "rgb(0,0,163)", "rgb(0,0,168)", "rgb(0,0,172)", "rgb(0,0,177)", "rgb(0,0,181)", "rgb(0,0,186)", "rgb(0,0,190)", "rgb(0,0,195)", "rgb(0,0,199)", "rgb(0,0,204)", "rgb(0,0,208)", "rgb(0,0,213)", "rgb(0,0,218)", "rgb(0,0,222)", "rgb(0,0,227)", "rgb(0,0,231)", "rgb(0,0,236)", "rgb(0,0,240)", "rgb(0,0,245)", "rgb(0,0,249)", "rgb(0,0,254)", "rgb(0,0,255)", "rgb(0,0,255)", "rgb(0,0,255)", "rgb(0,0,255)", "rgb(0,3,255)", "rgb(0,7,255)", "rgb(0,11,255)", "rgb(0,15,255)", "rgb(0,19,255)", "rgb(0,23,255)", "rgb(0,27,255)", "rgb(0,31,255)", "rgb(0,35,255)", "rgb(0,39,255)", "rgb(0,43,255)", "rgb(0,47,255)", "rgb(0,51,255)", "rgb(0,55,255)", "rgb(0,59,255)", "rgb(0,63,255)", "rgb(0,67,255)", "rgb(0,71,255)", "rgb(0,75,255)", "rgb(0,79,255)", "rgb(0,83,255)", "rgb(0,87,255)", "rgb(0,91,255)", "rgb(0,95,255)", "rgb(0,99,255)", "rgb(0,103,255)", "rgb(0,107,255)", "rgb(0,111,255)", "rgb(0,115,255)", "rgb(0,119,255)", "rgb(0,123,255)", "rgb(0,127,255)", "rgb(0,131,255)", "rgb(0,135,255)", "rgb(0,139,255)", "rgb(0,143,255)", "rgb(0,147,255)", "rgb(0,151,255)", "rgb(0,155,255)", "rgb(0,159,255)", "rgb(0,163,255)", "rgb(0,167,255)", "rgb(0,171,255)", "rgb(0,175,255)", "rgb(0,179,255)", "rgb(0,183,255)", "rgb(0,187,255)", "rgb(0,191,255)", "rgb(0,195,255)", "rgb(0,199,255)", "rgb(0,203,255)", "rgb(0,207,255)", "rgb(0,211,255)", "rgb(0,215,255)", "rgb(0,219,255)", "rgb(0,223,251)", "rgb(0,227,248)", "rgb(1,231,245)", "rgb(4,235,242)", "rgb(7,239,239)", "rgb(10,243,235)", "rgb(14,247,232)", "rgb(17,251,229)", "rgb(20,255,226)", "rgb(23,255,222)", "rgb(26,255,219)", "rgb(30,255,216)", "rgb(33,255,213)", "rgb(36,255,210)", "rgb(39,255,206)", "rgb(43,255,203)", "rgb(46,255,200)", "rgb(49,255,197)", "rgb(52,255,194)", "rgb(55,255,190)", "rgb(59,255,187)", "rgb(62,255,184)", "rgb(65,255,181)", "rgb(68,255,178)", "rgb(71,255,174)", "rgb(75,255,171)", "rgb(78,255,168)", "rgb(81,255,165)", "rgb(84,255,161)", "rgb(88,255,158)", "rgb(91,255,155)", "rgb(94,255,152)", "rgb(97,255,149)", "rgb(100,255,145)", "rgb(104,255,142)", "rgb(107,255,139)", "rgb(110,255,136)", "rgb(113,255,133)", "rgb(116,255,129)", "rgb(120,255,126)", "rgb(123,255,123)", "rgb(126,255,120)", "rgb(129,255,116)", "rgb(133,255,113)", "rgb(136,255,110)", "rgb(139,255,107)", "rgb(142,255,104)", "rgb(145,255,100)", "rgb(149,255,97)", "rgb(152,255,94)", "rgb(155,255,91)", "rgb(158,255,88)", "rgb(161,255,84)", "rgb(165,255,81)", "rgb(168,255,78)", "rgb(171,255,75)", "rgb(174,255,71)", "rgb(178,255,68)", "rgb(181,255,65)", "rgb(184,255,62)", "rgb(187,255,59)", "rgb(190,255,55)", "rgb(194,255,52)", "rgb(197,255,49)", "rgb(200,255,46)", "rgb(203,255,43)", "rgb(206,255,39)", "rgb(210,255,36)", "rgb(213,255,33)", "rgb(216,255,30)", "rgb(219,255,26)", "rgb(222,255,23)", "rgb(226,255,20)", "rgb(229,255,17)", "rgb(232,255,14)", "rgb(235,255,10)", "rgb(239,254,7)", "rgb(242,250,4)", "rgb(245,247,1)", "rgb(248,243,0)", "rgb(251,239,0)", "rgb(255,235,0)", "rgb(255,232,0)", "rgb(255,228,0)", "rgb(255,224,0)", "rgb(255,221,0)", "rgb(255,217,0)", "rgb(255,213,0)", "rgb(255,210,0)", "rgb(255,206,0)", "rgb(255,202,0)", "rgb(255,199,0)", "rgb(255,195,0)", "rgb(255,191,0)", "rgb(255,188,0)", "rgb(255,184,0)", "rgb(255,180,0)", "rgb(255,176,0)", "rgb(255,173,0)", "rgb(255,169,0)", "rgb(255,165,0)", "rgb(255,162,0)", "rgb(255,158,0)", "rgb(255,154,0)", "rgb(255,151,0)", "rgb(255,147,0)", "rgb(255,143,0)", "rgb(255,140,0)", "rgb(255,136,0)", "rgb(255,132,0)", "rgb(255,128,0)", "rgb(255,125,0)", "rgb(255,121,0)", "rgb(255,117,0)", "rgb(255,114,0)", "rgb(255,110,0)", "rgb(255,106,0)", "rgb(255,103,0)", "rgb(255,99,0)", "rgb(255,95,0)", "rgb(255,92,0)", "rgb(255,88,0)", "rgb(255,84,0)", "rgb(255,81,0)", "rgb(255,77,0)", "rgb(255,73,0)", "rgb(255,69,0)", "rgb(255,66,0)", "rgb(255,62,0)", "rgb(255,58,0)", "rgb(255,55,0)", "rgb(255,51,0)", "rgb(255,47,0)", "rgb(255,44,0)", "rgb(255,40,0)", "rgb(255,36,0)", "rgb(255,33,0)", "rgb(255,29,0)", "rgb(255,25,0)", "rgb(255,21,0)", "rgb(254,18,0)", "rgb(249,14,0)", "rgb(245,10,0)", "rgb(240,7,0)", "rgb(236,3,0)", "rgb(231,0,0)", "rgb(227,0,0)", "rgb(222,0,0)", "rgb(218,0,0)", "rgb(213,0,0)", "rgb(208,0,0)", "rgb(204,0,0)", "rgb(199,0,0)", "rgb(195,0,0)", "rgb(190,0,0)", "rgb(186,0,0)", "rgb(181,0,0)", "rgb(177,0,0)", "rgb(172,0,0)", "rgb(168,0,0)", "rgb(163,0,0)", "rgb(159,0,0)", "rgb(154,0,0)", "rgb(150,0,0)", "rgb(145,0,0)", "rgb(141,0,0)", "rgb(136,0,0)", "rgb(132,0,0)"];
         this.transform = 'lin';
         // put series options in options.series (dims, etc.)
-        this.dims = {xmin:0, xmax:1, ymin:0, ymax:1};
+        this.dims = {xmin:0, xmax:1, ymin:0, ymax:1, zmin:0, zmax: 1};
         this.transform = 'lin';
         $.extend(true, this, options);
         
@@ -28,13 +28,14 @@
         // need to create a canvas to draw on...
         this.canvas = new $.jqplot.GenericCanvas();
         this.canvas._plotDimensions = this._plotDimensions;
-        this._type = 'heatmap';
+        this._type = 'colorbar';
         
         var display_dims = options.display_dims || this.dims; // plot_dims = data_dims if not specified
         this._xaxis.min = display_dims.xmin;
         this._xaxis.max = display_dims.xmax;
-        this._yaxis.min = display_dims.ymin;
-        this._yaxis.max = display_dims.ymax;
+        this._yaxis.min = display_dims.zmin;
+        this._yaxis.max = display_dims.zmax;
+        //this._yaxis.ticks = generate_ticks({min:display_dims.zmin, max: display_dims.zmax}, this.transform);
         // kind of a hack, but prevents jqplot from messing with the data 
         // (if sortData=true were turned on, for instance);
         this.source_data = [];
@@ -42,15 +43,15 @@
             this.source_data.push(this.data[i].slice());
         }
         
-        this.data = [[this.dims.xmin, this.dims.ymin],
-                    [this.dims.xmax, this.dims.ymin],
-                    [this.dims.xmax, this.dims.ymax],
-                    [this.dims.xmin, this.dims.ymax]];
+        this.data = [[this.dims.xmin, this.dims.zmin],
+                    [this.dims.xmax, this.dims.zmin],
+                    [this.dims.xmax, this.dims.zmax],
+                    [this.dims.xmin, this.dims.zmax]];
         
-        this._plotData = [[this.dims.xmin, this.dims.ymin],
-                    [this.dims.xmax, this.dims.ymin],
-                    [this.dims.xmax, this.dims.ymax],
-                    [this.dims.xmin, this.dims.ymax]];
+        this._plotData = [[this.dims.xmin, this.dims.zmin],
+                    [this.dims.xmax, this.dims.zmin],
+                    [this.dims.xmax, this.dims.zmax],
+                    [this.dims.xmin, this.dims.zmax]];
                     
         // group: Methods 
         //
@@ -61,11 +62,12 @@
         this.zoom_to = zoom_to;
         this.set_transform(this.transform);
         this.update_plotdata();
+        
     };
     
             
     // call with scope of series    
-    $.jqplot.heatmapRenderer.prototype.get_sxdx = function(){
+    $.jqplot.colorbarRenderer.prototype.get_sxdx = function(){
         var xp = this._xaxis.u2p;
         var yp = this._yaxis.u2p;
         var dims = this.dims;
@@ -95,7 +97,7 @@
     };
     
     // call with scope of series
-    $.jqplot.heatmapRenderer.prototype.generate_cumsums = function () {
+    $.jqplot.colorbarRenderer.prototype.generate_cumsums = function () {
         var width = this.dims.xdim;
         var height = this.dims.ydim;
         var cumsum_x = [], cumsum_x_col;
@@ -119,43 +121,22 @@
     // called with scope of series
     // place rectangle of zoom-appropriate size for elements in source data.  
     // steps are big enough to skip completely-overlapping rectangles (saving time);
-    $.jqplot.heatmapRenderer.prototype.draw_rect = function (ctx, gd, options) {
+    $.jqplot.colorbarRenderer.prototype.draw_rect = function (ctx, gd, options) {
         // do stuff
-        var sxdx = this.get_sxdx();
-        var xzoom = sxdx.dw / sxdx.sw;
-        var yzoom = sxdx.dh / sxdx.sh;
-        var xstep = Math.max(1/xzoom, 1);
-        var ystep = Math.max(1/yzoom, 1);
-        var sx = parseInt(sxdx.sx), sy = parseInt(sxdx.sy);
-        var x0, y0, oldx0, oldy0, plotz;
         var width = ctx.canvas.width;
         var height = ctx.canvas.height;
-        var tzmax = this.t(this.dims.zmax);
-        if (sxdx.sw > 0 && sxdx.sh > 0) {
-            var zoom = 24;
-            ctx.clearRect(0,0, width, height);
-            var xmax = (sxdx.sw + sxdx.sx);
-            var ymax = (sxdx.sh + sxdx.sy);
-            var xw = Math.ceil(xzoom);
-            var yw = Math.ceil(yzoom);
-            for (var x=sx;x<xmax;x+=xstep){
-                var xindex = parseInt(x);
-                var scol = this.plotdata[xindex];
-                x0 = Math.round(sxdx.dx + (x-sxdx.sx)*xzoom);
-		        for (var y=sy;y<ymax;y+=ystep){
-			        plotz = scol[parseInt(y)];
-			        y0 = Math.round(sxdx.dy + (y-sxdx.sy)*yzoom);
-		            //ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-		            ctx.fillStyle = this.palette_str[plotz];
-		            ctx.fillRect(x0,y0,xw,yw);
-		        }
-	        }
-        }
+        var rh = height/256.0;
+        ctx.clearRect(0,0, width, height);
+        for (var i=0; i<256; i++) {
+            var y0 = Math.floor(i*rh);
+            ctx.fillStyle = this.palette_str[255-i];
+            ctx.fillRect(0, y0, width, Math.ceil(rh));
+        } 
     };
     
     // draw directly on pixel buffer, then blit to screen:
     // uses reverse-lookup to grab z-value in source data. (no interpolation or averaging).
-    $.jqplot.heatmapRenderer.prototype.draw_blit = function (ctx, gd, options) {
+    $.jqplot.colorbarRenderer.prototype.draw_blit = function (ctx, gd, options) {
         var width = ctx.canvas.width;
         var height = ctx.canvas.height;
         ctx.clearRect(0,0, width, height);
@@ -184,7 +165,7 @@
         ctx.putImageData(myImageData, 0,0);
     };
     
-    $.jqplot.heatmapRenderer.prototype.draw = $.jqplot.heatmapRenderer.prototype.draw_blit;
+    $.jqplot.colorbarRenderer.prototype.draw = $.jqplot.colorbarRenderer.prototype.draw_rect;
     
     function add_image(data) {
         var canvas = document.createElement('canvas');
@@ -268,8 +249,6 @@
         var width = this.dims.xdim;
         var height = this.dims.ydim;
         var tzmax = this.t(this.dims.zmax);
-        var tzmin = this.t(this.dims.zmin);
-        if (isNaN(tzmin)) tzmin = 0;
         var data = this.source_data; 
         var plotdata = [], datacol;      
         
@@ -279,7 +258,7 @@
                 var offset = 4*((r*width) + c);
                 //var z = data[c][height-r-1];
                 var z = data[c][r];
-                var plotz = Math.floor(((this.t(z) - tzmin) / tzmax) * 255.0);
+                var plotz = Math.floor((this.t(z) / tzmax) * 255.0);
 
                 plotz = ((plotz>255)? 255 : plotz);
                 plotz = ((plotz<0)? 0 : plotz);
@@ -288,6 +267,69 @@
             plotdata.push(datacol.slice());
         }
         this.plotdata = plotdata;
+    };
+    
+    function bestLinearInterval(range) {
+        var expv = Math.floor(Math.log(range)/Math.LN10);
+        var magnitude = Math.pow(10, expv);
+        var f = range / magnitude;
+
+        if (f<=1.6) {return 0.2*magnitude;}
+        if (f<=4.0) {return 0.5*magnitude;}
+        if (f<=8.0) {return magnitude;}
+        return 2*magnitude; 
+    };
+    
+    function nextLogTick(val) {
+        // finds the next log tick above the current value,
+        // using 1, 2, 5, 10, 20, ... scaling
+        var expv = Math.floor(Math.log(val)/Math.LN10);
+        var magnitude = Math.pow(10, expv);
+        var f = val / magnitude;
+
+        if (f<1.0) {return 1.0*magnitude;}
+        if (f<2.0) {return 2.0*magnitude;}
+        if (f<5.0) {return 5.0*magnitude;}
+        return 10*magnitude; 
+    };
+    
+    function mod(a,b) {
+        return a % b < 0 ? b + a % b : a % b
+    };
+    
+    function generate_ticks(ticklimits, transform) {
+        var transform = transform || 'lin';
+        var min = ticklimits.min,
+            max = ticklimits.max;
+        var ticks = [];
+        if (transform == 'lin') {
+            var tickInterval = bestLinearInterval(max-min);
+            ticks.push([min, ' ']);
+            var tick = min - mod(min, tickInterval) + tickInterval;
+            while (tick < max) {
+                if (Math.abs(tick) < 1e-13) tick = 0;            
+                ticks.push(tick);
+                tick += tickInterval;
+            }
+            ticks.push([max,' ']);
+        } 
+        else if (transform == 'log') {
+            var lmax = Math.log(max)/Math.LN10;
+            ticks.push([Math.log(min)/Math.LN10, ' ']);
+            var tick = nextLogTick(min);
+            while( tick < max ) {
+                ticks.push([Math.log(tick)/Math.LN10, tick]);
+                tick = nextLogTick(tick);
+            }
+            ticks.push([Math.log(max)/Math.LN10, ' ']);
+        } 
+        else {
+            // unknown transform - return max and min
+            ticks.push(min);
+            ticks.push(max);
+        }
+        
+        return ticks; 
     };
        
 })(jQuery);
