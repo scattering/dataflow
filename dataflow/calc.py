ae01f43f29351a39465607e403b1b58c6ab7318e
William Ratcliff <william.ratcliff@gmail.com>
2012-01-03 14:54:02 -0500

    modified calc.py for windows (redis)

--- dataflow/calc.py
+++ dataflow/calc.py
@@ -11,7 +11,8 @@ import hashlib, redis, types, os
 from copy import deepcopy
 import numpy
 
-os.system("redis-server") # ensure redis is running
+if not sys.platform=='win32':
+    os.system("redis-server") # ensure redis is running
 server = redis.Redis("localhost")
 #if not hasattr(server, 'rpush'): server.rpush = server.push
 
