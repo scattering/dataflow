d6a06744677a197c25f8aec75dcf563ac6399bf2
Brian Maranville <brian.maranville@nist.gov>
2012-04-26 13:10:34 -0400

    blanking sliceplot graph while loading

--- dataflow/offspecular/instruments.py
+++ dataflow/offspecular/instruments.py
@@ -125,14 +125,19 @@ def _load_data(name, auto_PolState, PolState):
     return LoadICPData(fileName, path=dirName, auto_PolState=auto_PolState, PolState=PolState)
 
 
-def load_asterix_action(files=[], **kwargs):
+def load_asterix_action(input=[], files=[], **kwargs):
     result = []
     for f in files:
         subresult = _load_asterix_data(f)
         if type(subresult) == types.ListType:
             result.extend(subresult)
         else:
-            result.append(subresult)   
+            result.append(subresult)
+    for subresult in input:
+        if type(subresult) == types.ListType:
+            result.extend(subresult)
+        else:
+            result.append(subresult)
     #result = [_load_data(f, auto_PolState, PolStates.get(get_friendly_name(os.path.split(f)[-1]), '')) for f in files] # not bundles
     return dict(output=result)
     
@@ -175,8 +180,10 @@ PolStates_field = {
 load = load_module(id='ospec.load', datatype=OSPEC_DATA,
                    version='1.0', action=load_action, fields={'auto_PolState': auto_PolState_field, 'PolStates': PolStates_field, 'autochain-loader':autochain_loader_field}, filterModule=LoadICPData)
 
-load_asterix = load_asterix_module(id='ospec.asterix.load', datatype=OSPEC_DATA,
-                   version='1.0', action=load_asterix_action, filterModule=LoadAsterixRawHDF)
+#load_asterix = load_asterix_module(id='ospec.asterix.load', datatype=OSPEC_DATA,
+#                   version='1.0', action=load_asterix_action, filterModule=LoadAsterixRawHDF)
+load_asterix = load_module(id='ospec.asterix.load', datatype=OSPEC_DATA,
+                   version='1.0', action=load_asterix_action, fields={'autochain-loader':autochain_loader_field}, filterModule=LoadAsterixRawHDF)
                    
 load_asterix_spectrum = load_asterix_spectrum_module(id='ospec.asterix.load_spectrum', datatype=OSPEC_DATA,
                     version='1.0', action=load_asterix_spectrum_action)
