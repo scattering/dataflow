1bfa5edb95fb264294e73802e65af90b0a7648b5
David Chu <chumeister05@gmail.com>
2012-07-19 13:03:31 -0400

    Adding VerticalLineInteractor plugin

--- static/lib/interactors/linear_interactor_plugin.js
+++ static/lib/interactors/linear_interactor_plugin.js
@@ -16,24 +16,24 @@
     $.jqplot.InteractorPluginSubtypes.Line = $.jqplot.LinearInteractorPlugin;
     
     $.jqplot.LinearInteractorPlugin.prototype.init = function(options) {
-            $.jqplot.InteractorPlugin.prototype.init.call(this, options);
-            this.xmin = 0.0;
-            this.ymin = 0.0;
-            this.xmax = 1.0;
-            this.ymax = 1.0;
-            this.slope = 1.0;
-            $.extend(this, options);
-            this.slope = (this.ymax - this.ymin) / (this.xmax - this.xmin);
-            this.intercept = (this.ymax - this.ymin) - (this.slope * (this.xmax - this.xmin));
+        $.jqplot.InteractorPlugin.prototype.init.call(this, options);
+        this.xmin = 0.0;
+        this.ymin = 0.0;
+        this.xmax = 1.0;
+        this.ymax = 1.0;
+        this.slope = 1.0;
+        $.extend(this, options);
+        this.slope = (this.ymax - this.ymin) / (this.xmax - this.xmin);
+        this.intercept = (this.ymax - this.ymin) - (this.slope * (this.xmax - this.xmin));
         
-            this.p1 = new $.jqplot.PluginPoint(); this.p1.initialize(this, this.xmin, this.ymin);
-            this.p2 = new $.jqplot.PluginPoint(); this.p2.initialize(this, this.xmax, this.ymax);
+        this.p1 = new $.jqplot.PluginPoint(); this.p1.initialize(this, this.xmin, this.ymin);
+        this.p2 = new $.jqplot.PluginPoint(); this.p2.initialize(this, this.xmax, this.ymax);
         
-            this.linear = new $.jqplot.Linear(); this.linear.initialize(this, this.p1, this.p2, 4);
+        this.linear = new $.jqplot.Linear(); this.linear.initialize(this, this.p1, this.p2, 4);
         
-            this.grobs.push(this.linear, this.p1, this.p2);
+        this.grobs.push(this.linear, this.p1, this.p2);
         
-            //this.redraw();              
+        //this.redraw();              
     }
     
 })(jQuery);
