2114888f4e0421de38ec2d34ac1c19bfacdfca8b
David Chu <chumeister05@gmail.com>
2012-07-20 17:05:13 -0400

    Added footprint correcion module and files related to reflectometry filters.py

--- dataflow/refl/instruments.py
+++ dataflow/refl/instruments.py
@@ -6,31 +6,39 @@ from ..core import Instrument, Datatype, register_module
 from ..modules.join import join_module
 from ..modules.scale import scale_module
 from ..modules.normalize import normalize_module
-from ..modules.footprint import footprint
+from ..modules.footprintmodule import footprint_module
 
-def join_action(input=None):
-    print "combining", input
-    result = None
-    return dict(output=result)
-join = join_module(id='refl.join', datatype='data1d.refl', \
-                   version='1.0', action=join_action)
+#def join_action(input=None):
+#    print "combining", input
+#    result = None
+#    return dict(output=result)
+#join = join_module(id='refl.join', datatype='data1d.refl', \
+#                   version='1.0', action=join_action)
 
-def scale_action(input=None, scale=None):
-    print "scale", input, "by", scale
-    result = None
-    return dict(output=result)
-scale = scale_module(id='refl.scale', datatype='data1d.refl',
-                     version='1.0', action=scale_action)
+#def scale_action(input=None, scale=None):
+#    print "scale", input, "by", scale
+#    result = None
+#    return dict(output=result)
+#scale = scale_module(id='refl.scale', datatype='data1d.refl',
+#                     version='1.0', action=scale_action)
 
-def subtract_action(input=None):
-    result = None
-    return dict(output=result)
-subtract = scale_module(id='refl.subtract', datatype='data1d.refl',
-                     version='1.0', action=subtract_action)
+#def subtract_action(input=None):
+#    result = None
+#    return dict(output=result)
+#subtract = scale_module(id='refl.subtract', datatype='data1d.refl',
+#                     version='1.0', action=subtract_action)
 
-def normalize_action(input=None):
-    result = None
-    return dict(output=result)
+#def normalize_action(input=None):
+#    result = None
+#    return dict(output=result)
+
+# FootprintCorrection module
+def footprint_action(input=[], start="", end="", slope="", intercept=""):
+    print "applying footprint correction"
+    return dict(output=FootprintCorrection().apply(input, start, end, slope, intercept)) 
+    footprint = footprint_module(id='refl.footprint', datatype='data1d.refl',
+                    version='1.0', action=footprint_action)
+    
 normalize = normalize_module(id='refl.normalize', datatype='data1d.refl',
                              version='1.0', action=normalize_action)
 
@@ -39,11 +47,12 @@ data1d = Datatype(id='data1d.refl',
                   plot='reflplot')
 
 input = [load]
-reduction = [join, scale, subtract, normalize, footprint, polcor]
+#reduction = [join, scale, subtract, normalize, footprint, polcor]
+reduction = [footprint]
 
-NG1 = Instrument(id='ncnr.refl.ng1',
-                 name='NCNR NG1',
-                 archive='http://www.ncnr/nist.gov/data/ng1',
+PBR = Instrument(id='ncnr.refl.pbr',
+                 name='NCNR PBR',
+                 archive='http://www.ncnr/nist.gov/data/pbr',
                  menu=[
                        ('Input', input),
                        ('Reduction', reduction),
@@ -52,4 +61,4 @@ NG1 = Instrument(id='ncnr.refl.ng1',
                  datatypes=[data1d],
                  )
 
-instruments = [NG1]
+instruments = [PBR]
