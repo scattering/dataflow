ebc8905b7f6cc44a320c4813e27beb8384e49f71
Paul Kienzle <pkienzle@nist.gov>
2014-02-14 15:21:21 -0500

    move ubmatrix c code to sleep

--- manage.py
+++ manage.py
@@ -7,11 +7,9 @@ sys.path.insert(0, path)
 
 # Make sure UB matrix calculator is compiled and on the path
 sys.dont_write_bytecode = True
-import run_helper as rh
 
 # Run django
 if __name__ == "__main__":
-    rh.build('reduction/tas/ubmatrix')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apps.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
--- test.py
+++ test.py
@@ -14,6 +14,4 @@ sys.dont_write_bytecode = True
 sys.path.insert(0,os.path.dirname(__file__))
 import run_helper as rh
 
-# rh.build()  # uncomment if program has compiled components
-rh.build('reduction/tas/ubmatrix')
 rh.nose(['reduction', 'dataflow', 'test/sample_reduction.py'], docext=None)
