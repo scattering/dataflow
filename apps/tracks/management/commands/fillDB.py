2c0cb36715bd24304f0adf2c6bc5c7198c180fa5
Paul Kienzle <pkienzle@nist.gov>
2014-02-24 11:10:26 -0500

    autocreate admin/admin for debug mode

--- apps/tracks/management/commands/fillDB.py
+++ apps/tracks/management/commands/fillDB.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         set_instruments()
         set_domain()
+        set_default_admin_user()
 
 FACILITIES = {
     'NCNR': {
@@ -27,20 +28,37 @@ FACILITIES = {
     },
 }
 
+def set_default_admin_user():
+    from django.conf import settings
+    from django.contrib.auth import models as auth_models
+    from django.contrib.auth.management import create_superuser
+
+    if not settings.DEBUG:
+        return
+    try:
+        auth_models.User.objects.get(username='admin')
+    except auth_models.User.DoesNotExist:
+        print '*' * 80
+        print 'Creating admin user -- login: admin, password: admin'
+        print '*' * 80
+        assert auth_models.User.objects.create_superuser('admin', '', 'admin')
+    else:
+        print 'Admin user already exists.'
+
+
 def set_instruments():
     from apps.tracks.models import Instrument, Facility
 
     for facility_name,instrument_group in FACILITIES.items():
         facility = Facility.objects.create(name=facility_name)
-        for type,instruments in instrument_group.items():
+        for itype,instruments in instrument_group.items():
             for beamline,name in instruments.items():
-                Instrument.objects.create(name=name, type=type,
+                Instrument.objects.create(name=name, instrument_type=itype,
                                           facility=facility,
                                           beamline=beamline)
     # Need to plug in initial templates and calibration files
 
 def set_domain():
-    from django.contrib.sites.models import RequestSite
     from django.contrib.sites.models import Site
     from django.conf import settings
 
--- resetdb.sh
+++ resetdb.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-
-
+# Remove the default user database for the debug user
 rm /tmp/tracks/user.db
-python manage.py syncdb
+
+# Create an empty database.  The --noinput suppresses the request to create
+# an admin user, which is instead created by the filldb command.
+python manage.py syncdb --noinput
+
+# Fill in the instruments by running apps.tracks.management.commands.filldb
+# In debug mode, this also creates the default admin user
+python manage.py filldb
+
+# User profile management through userena uses migrate to create its tables
 python manage.py migrate
+
+# userena requires check_permissions to set the correct table permissions
 python manage.py check_permissions
-python manage.py fillDB
